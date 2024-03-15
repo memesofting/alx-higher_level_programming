56f8b893158f9d9cdf8fd63262b31bf8bc2e2b53
memesofting <bibitayoadekunle00@gmail.com>
2024-03-15 01:29:37 +0100

    moving to pc

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,1 +1,12 @@
 #!/usr/bin/python3
+if __name__ == "__main__":
+    from sys import argv
+    numarg = len(argv) - 1
+    if numarg == 0:
+        print("0 arguments.")
+    elif numarg == 1:
+        print("{} argument.".format(numarg))
+    else:
+        print("{} arguments:".format(numarg))
+    for a in range(numarg):
+        print("{}: {}".format(a + 1, argv[a + 1]))

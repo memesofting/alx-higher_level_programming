af34c081f9e43c7859055751a5cb57161fb6e55d
memesofting <bibitayoadekunle00@gmail.com>
2024-03-15 09:49:50 +0100

    add to infinity

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,1 +1,8 @@
 #!/usr/bin/python3
+if __name__ == "__main__":
+    from sys import argv
+    numargs = len(argv) - 1
+    sum = 0
+    for i in range(numargs):
+        sum += int(argv[i + 1])
+    print("{}".format(sum))

7e942fd886752c56d0e991bac9cdae7e8a082ee6
memesofting <bibitayoadekunle00@gmail.com>
2024-03-19 15:54:29 +0100

    switching to phone

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,4 +1,9 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
     if matrix:
+        for i in range(len(matrix)):
+            row = matrix[i]
+            for j in range(len(row)):
+                if j == len(row) -1:
+                print("{:d}".format(row{j}))
 

2cfff76e167eaf8b1ce02e7f6506189f83910531
memesofting <bibitayoadekunle00@gmail.com>
2024-03-20 17:29:43 +0100

    switching to phone

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,1 +1,9 @@
 #!/usr/bin/python3
+def square_matrix_simple(matrix=[]):
+    copy = []
+    if matrix != None:
+        copy = matrix.copy()
+    for i in range(len(matrix)):
+        for j in matrix[i]:
+            copy.append(j ** 2)
+    return copy

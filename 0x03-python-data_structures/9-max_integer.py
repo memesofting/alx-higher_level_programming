d716e97783ee2521c3cd189d51777bd12281ce4e
memesofting <bibitayoadekunle00@gmail.com>
2024-03-20 02:23:46 +0000

    max integer for empty list

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
-    if my_list:
+    if my_list == []:
+        return None
+    else:
         max_n = my_list[0]
         for i in range(1, len(my_list)):
             if my_list[i] > max_n:

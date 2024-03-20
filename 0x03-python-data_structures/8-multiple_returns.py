fb7696373a25f2d2989a41fa49868c7450522e3f
memesofting <bibitayoadekunle00@gmail.com>
2024-03-20 01:35:04 +0000

    multiple returns

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,1 +1,8 @@
 #!/usr/bin/python3
+def multiple_returns(sentence):
+    len_s = len(sentence)
+    if len_s == 0:
+        sentence[0] = None
+    else:
+        tup_s = (len_s, sentence[0])
+    return tup_s

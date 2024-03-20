73e6c804d72e75db5681b974048304190e170eb3
memesofting <bibitayoadekunle00@gmail.com>
2024-03-20 23:47:34 +0100

    search and replace list element

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,1 +1,10 @@
 #!/usr/bin/python3
+def search_replace(my_list, search, replace):
+    new_list = []
+    if my_list is not None:
+        for i in my_list:
+            if i == search:
+                new_list.append(replace)
+            else:
+                new_list.append(i)
+    return new_list

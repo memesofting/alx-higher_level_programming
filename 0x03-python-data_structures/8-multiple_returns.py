ad07a427822d93bae3072fad72fdce363de5fdcc
memesofting <bibitayoadekunle00@gmail.com>
2024-03-20 01:49:56 +0000

    handled empty string in 8-multiple returns

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
     len_s = len(sentence)
-    if len_s == 0:
-        sentence[0] = None
+    if sentence == "":
+        tup_s = (0, None)
+        return tup_s
     else:
         tup_s = (len_s, sentence[0])
-    return tup_s
+        return tup_s

49bf1418d1a347b48038021bfee5f0ed08fb74a6
memesofting <bibitayoadekunle00@gmail.com>
2024-03-21 00:19:48 +0100

    add unique list elements

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,1 +1,9 @@
 #!/usr/bin/python3
+def uniq_add(my_list=[]):
+    sum_uniq = set()
+    total = 0
+    for i in my_list:
+        sum_uniq.add(i)
+    for n in sum_uniq:
+        total += n
+    return total

416ba292dd336daeacd5930c52959a036d0ae34a
memesofting <bibitayoadekunle00@gmail.com>
2024-03-20 01:17:39 +0000

    add tuples

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,4 +1,19 @@
 #!/usr/bin/pytho3
 def add_tuple(tuple_a=(), tuple_b=()):
     result = ()
-    if tuple_a < 2
+    len_a = len(tuple_a)
+    len_b = len(tuple_b)
+    if len_a < 1:
+        tuple_a = (0, 0)
+    elif len_a == 1:
+        tuple_a = (tuple_a[0], 0)
+    else:
+        tuple_a = (tuple_a[0], tuple_a[1])
+    if len_b < 1:
+        tuple_b = (0, 0)
+    elif len_b == 1:
+        tuple_b = (tuple_b[0], 0)
+    else:
+        tuple_b = (tuple_b[0], tuple_b[1])
+    result = (tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1])
+    return result

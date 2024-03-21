b4bc7800366f46898c8af986f4833fb51c6465d7
memesofting <bibitayoadekunle00@gmail.com>
2024-03-21 11:04:26 +0100

    dictionary operations

--- 0x04-python-more_data_structures/8-simple_delete.py
+++ 0x04-python-more_data_structures/8-simple_delete.py
@@ -1,1 +1,9 @@
 #!/usr/bin/python3
+def simple_delete(a_dictionary, key=""):
+    if a_dictionary is None:
+        return None
+    if key not in a_dictionary:
+        return a_dictionary
+    else:
+        del a_dictionary[key]
+    return a_dictionary

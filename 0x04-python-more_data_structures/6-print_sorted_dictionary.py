e408da5bbea2e5e9006078caa6677c1671803185
memesofting <bibitayoadekunle00@gmail.com>
2024-03-21 13:27:37 +0100

    sorted

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 if __name__ == "6-print_sorted_dictionary":
     def print_sorted_dictionary(a_dictionary):
-        sort_dict = sorted(a_dictionary)
-        for key in sort_dict:
-            print(key, ":", a_dictionary[key])
+        for name in sorted(a_dictionary):
+            print(name, ":", a_dictionary[name])

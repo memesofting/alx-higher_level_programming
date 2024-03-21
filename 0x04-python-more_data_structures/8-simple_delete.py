e8bf5541f8d234ec3fc6c61fa408e479fdc9b52c
memesofting <bibitayoadekunle00@gmail.com>
2024-03-21 13:31:30 +0100

    delete key

--- 0x04-python-more_data_structures/8-simple_delete.py
+++ 0x04-python-more_data_structures/8-simple_delete.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
-def simple_delete(a_dictionary, key=""):
-    if a_dictionary is None:
-        return None
-    if key in a_dictionary:
-        del a_dictionary[key]
-    return a_dictionary
+if __name__ == "8-simple_delete":
+    def simple_delete(a_dictionary, key=""):
+        if a_dictionary is None:
+            return None
+        if key in a_dictionary:
+            del a_dictionary[key]
+        return a_dictionary

1f1544385a3a4b354c31c88d6f1073f1aef6b0fb
memesofting <bibitayoadekunle00@gmail.com>
2024-03-21 13:16:41 +0100

    solving name import issues

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-def print_sorted_dictionary(a_dictionary):
-    sort_dict = sorted(a_dictionary)
-    for key in sort_dict:
-        print(key, ":", a_dictionary[key])
+if __name__ == "6-print_sorted_dictionary":
+    def print_sorted_dictionary(a_dictionary):
+        sort_dict = sorted(a_dictionary)
+        for key in sort_dict:
+            print(key, ":", a_dictionary[key])

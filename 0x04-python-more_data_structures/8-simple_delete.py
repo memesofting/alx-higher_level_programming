748d537fd6fef608567121aee85612cef6f4c4be
memesofting <bibitayoadekunle00@gmail.com>
2024-03-21 11:33:07 +0100

    correcting dict delete

--- 0x04-python-more_data_structures/8-simple_delete.py
+++ 0x04-python-more_data_structures/8-simple_delete.py
@@ -2,8 +2,6 @@
 def simple_delete(a_dictionary, key=""):
     if a_dictionary is None:
         return None
-    if key not in a_dictionary:
-        return a_dictionary
-    else:
+    if key in a_dictionary:
         del a_dictionary[key]
     return a_dictionary

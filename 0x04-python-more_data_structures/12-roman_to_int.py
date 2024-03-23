c318c0fef98926a2f02039d04fb2af5df498a8ab
memesofting <bibitayoadekunle00@gmail.com>
2024-03-23 01:31:56 +0000

    on rom numbers

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,1 +1,7 @@
 #!/usr/bin/python3
+def roman_to_int(roman_string):
+    if roman_string not str or roman_string is None:
+        return 0
+    else:
+        for i in range(len(roman_string)):
+            if roman_string

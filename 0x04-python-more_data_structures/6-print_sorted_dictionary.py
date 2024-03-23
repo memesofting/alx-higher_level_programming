047bc080c971df582eff441a7d3509c1cd1620b4
memesofting <bibitayoadekunle00@gmail.com>
2024-03-23 08:27:47 +0000

    sort correction

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -2,5 +2,4 @@
 if __name__ == "6-print_sorted_dictionary":
     def print_sorted_dictionary(a_dictionary):
         for name in sorted(a_dictionary):
-            # print(name, ":", a_dictionary[name])
-            print("{}:{}".format(name, a_dictionary[name]))
+            print("{}: {}".format(name, a_dictionary[name]))

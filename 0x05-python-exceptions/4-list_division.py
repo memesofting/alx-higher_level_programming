d580eaad64b8f83f60292cd8eeefd2181cfc977c
memesofting <bibitayoadekunle00@gmail.com>
2024-04-02 18:47:34 +0100

    correcting list division

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
 def list_division(my_list_1, my_list_2, list_length):
     new_list = []
-    result = 0
+    if my_list_1 is None or my_list_2 is None:
+        return None
     if my_list_1 is not None and my_list_2 is not None:
         for i in range(list_length):
+            result = 0
             try:
                 result = my_list_1[i] / my_list_2[i]
             except TypeError:

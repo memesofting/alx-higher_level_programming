18a7ba7f6968306528128fd1e5531d30f17464a0
memesofting <bibitayoadekunle00@gmail.com>
2024-04-02 23:46:04 +0100

    print error to stderr

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -7,4 +7,5 @@ def safe_print_integer_err(value):
         print("{:d}".format(value))
         return True
     except ValueError:
+        sys.stderr.write("{} is not an integer".format(value))
         return False

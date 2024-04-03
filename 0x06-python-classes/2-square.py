eab8246173268fed311215e367d112c4e164c91b
memesofting <bibitayoadekunle00@gmail.com>
2024-04-03 22:04:27 +0100

    square raises exception

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""Square module modified"""
+
+
+class Square:
+    """Square class raises exception"""
+    def __init__(self, size=0):
+        self.size = size
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value

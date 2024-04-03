fd0d83eacd43358a0a8485532540b76695b7889d
memesofting <bibitayoadekunle00@gmail.com>
2024-04-03 23:27:59 +0100

    print square

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""Square module modified"""
+
+
+class Square:
+    """Square class raises exception"""
+    def __init__(self, size=0):
+        self.size = size
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
+
+    def area(self):
+        """method gets the area of squares"""
+        result = self.size ** 2
+        return result
+
+    def my_print(self):
+        if self.size == 0:
+            print()
+        else:
+            for i in range(self.size):
+                for j in range(self.size):
+                    print("#", end='')
+                print()

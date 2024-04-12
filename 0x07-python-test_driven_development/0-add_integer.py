e0d209505d1bd5598366515596aace5958a533c3
memesofting <bibitayoadekunle00@gmail.com>
2024-04-12 08:52:58 +0100

    testing doc

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,12 +1,28 @@
 #!/usr/bin/python3
+"""This module adds two integers"""
+
+
 def add_integer(a, b=98):
     """
     Adds two intwgers and returns an integer
+    Args:
+        a: first integer
+        b: second integer
+    Returns:
+        The sum of two integers
     """
-    try:
+    if isinstance(a, float):
         a = int(a)
+    if isinstance(b, float):
         b = int(b)
-    except(TypeError):
-
+    if not isinstance(a, int):
+        raise TypeError("a must be an integer")
+    if not isinstance(b, int):
+        raise TypeError("b must be an integer")
     result = a + b
     return result
+
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod()

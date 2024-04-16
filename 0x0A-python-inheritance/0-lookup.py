e7cb7388cff9532e4ad5d43556fa36731ead4df1
memesofting <bibitayoadekunle00@gmail.com>
2024-04-16 13:11:22 +0100

    read file

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,3 +1,13 @@
 #!/usr/bin/python3
+"""This module looks-up an oject"""
+
+
 def lookup(obj):
+    """
+    This function gets the available attributes of an object
+    Args:
+        obj: object
+    Returns:
+        List of available attributes and methods of an object
+    """
     return dir(obj)
---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""THis module reads a text file"""
+
+
+def read_file(filename=""):
+    """
+    This function opens and reads a text file
+    Args:
+        filename: file to be read
+    Returns:
+        string from file
+    """
+
+    with open(filename, "r", encoding="utf-8") as f:
+        print(f.read())

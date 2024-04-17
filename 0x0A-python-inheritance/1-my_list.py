fc12ab5ffa0991bde1146bbacaaa9fec7c2737ed
memesofting <bibitayoadekunle00@gmail.com>
2024-04-17 14:29:12 +0100

    inherit from inbuilt list class and print sorted list

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+Module contains class that inherits from list
+"""
+
+
+class MyList(list):
+    """MyList class that inherits from the list class"""
+
+    def print_sorted(self):
+        """
+        Prints a sorted list in ascending order
+        Returns:
+            Sorted list
+        """
+
+        list_sorted = sorted(self)
+        print(list_sorted)
+
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testfile()

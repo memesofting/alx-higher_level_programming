95ca49af8d40beb83adc9f5f673e04be822c14f9
memesofting <bibitayoadekunle00@gmail.com>
2024-04-18 09:15:46 +0100

    created square class

--- 0x0A-python-inheritance/9-main.py
+++ 0x0A-python-inheritance/9-main.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 Rectangle = __import__('9-rectangle').Rectangle
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 r = Rectangle(3, 5)
 
 print(r)
 print(r.area())
+print(issubclass(Rectangle, BaseGeometry))

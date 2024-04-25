955c8ebdbee022f5a736f1f03797a2ded1620a07
memesofting <bibitayoadekunle00@gmail.com>
2024-04-25 13:56:40 +0100

    square test documentation

--- 0x0C-python-almost_a_circle/tests/test_models/test_square.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_square.py
@@ -5,6 +5,7 @@ from models.square import Square
 
 class TestSquare(unittest.TestCase):
     """Square class test cases"""
+
     def test_square_area(self):
         """test for square area"""
         s1 = Square(5)
@@ -13,14 +14,16 @@ class TestSquare(unittest.TestCase):
         self.assertEqual(s1.area(), 25)
         self.assertEqual(s2.area(), 4)
         self.assertEqual(s3.area(), 9)
-    
+
     def test_str(self):
         """Test __str__override"""
         s1 = Square(5)
         self.assertEqual(str(s1), "[Square] (5) 0/0 - 5")
-        
+
     def test_size(self):
+        """Test square size"""
         self.assertRaises(Square("9"), "[TypeError] width must be an integer")
-        
+
+
 if __name__ == "__main__":
     unittest.main()

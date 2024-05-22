3457cffee069b1d7c8e44d7f3ed79f74a56656d7
memesofting <bibitayoadekunle00@gmail.com>
2024-05-22 19:02:40 +0100

    list all tables

--- 0x0D-SQL_introduction/2-remove_database.sql
+++ 0x0D-SQL_introduction/2-remove_database.sql
@@ -1,2 +1,2 @@
 -- deletes a database hbtn_0c_0 without failing if it does not exist
-DELETE DATABASE IF NOT EXISTS hbtn_0c_0
+DROP DATABASE IF NOT EXISTS hbtn_0c_0
---
+++ 0x0D-SQL_introduction/3-list_tables.sql
@@ -0,0 +1,2 @@
+-- script lists all the tables of a database in MySQL server
+SHOW TABLES

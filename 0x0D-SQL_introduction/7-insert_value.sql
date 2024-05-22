c80f098aa03e8aa27715566c4903d1febce8924f
memesofting <bibitayoadekunle00@gmail.com>
2024-05-22 20:14:37 +0100

    inserting rows

--- 0x0D-SQL_introduction/7-insert_value.sql
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -1,4 +1,3 @@
 -- inserts a new row in table hbtn_0c_0
-USE hbtn_0c_0;
-INSERT INTO first_table
-VALUES (id 89, name Best School)
+-- USE hbtn_0c_0;
+INSERT INTO first_table (id, name) VALUES (89, Best School)

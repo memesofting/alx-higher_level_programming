b43fb571ae76f4058474810a5893e779ea7df18c
memesofting <bibitayoadekunle00@gmail.com>
2024-05-22 19:18:15 +0100

    create first table

--- 0x0D-SQL_introduction/2-remove_database.sql
+++ 0x0D-SQL_introduction/2-remove_database.sql
@@ -1,2 +1,2 @@
 -- deletes a database hbtn_0c_0 without failing if it does not exist
-DROP DATABASE IF NOT EXISTS hbtn_0c_0
+DROP DATABASE IF EXISTS hbtn_0c_0;
--- 0x0D-SQL_introduction/3-list_tables.sql
+++ 0x0D-SQL_introduction/3-list_tables.sql
@@ -1,2 +1,2 @@
 -- script lists all the tables of a database in MySQL server
-SHOW TABLES
+SHOW TABLES;

2d9bc13c357be86523ac4b3440f79f7186b795ae
memesofting <bibitayoadekunle00@gmail.com>
2024-05-23 17:40:24 +0000

    list top scores

---
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -0,0 +1,4 @@
+-- list all records of the table 'second_table'displaying score and name in ascending order
+SELECT score, name
+FROM second_table
+ORDER BY score DSC
---
+++ 0x0D-SQL_introduction/11-best_score.sql
@@ -0,0 +1,5 @@
+-- list all records with a score greater than 10
+SELECT score, name
+FROM second_table
+WHERE score >= 10
+ORDER BY score DSC

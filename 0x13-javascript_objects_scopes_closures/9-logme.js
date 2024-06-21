3ac2e3feffb6d48822cebea74cb3192f33ee1634
memesofting <bibitayoadekunle00@gmail.com>
2024-06-21 20:25:20 +0100

    log arguments

---
+++ 0x13-javascript_objects_scopes_closures/9-logme.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+
+// script prints the number of arguments already printed and
+// the new argument value
+let count = 0;
+exports.logMe = function (item) {
+  console.log(count + ': ' + item);
+  count++;
+};

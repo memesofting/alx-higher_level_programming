a12ba83fd245c33c8601ed4144fe05529a550477
memesofting <bibitayoadekunle00@gmail.com>
2024-06-21 12:46:28 +0100

    square class with charPrint method

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,21 @@
+#!/usr/bin/node
+
+const Rectangle = require('./4-rectangle');
+
+class Square extends Rectangle {
+  constructor (size) {
+    super(size, size);
+  }
+
+  charPrint (c) {
+    for (let i = 0; i < this.height; i++) {
+      let result = '';
+      for (let j = 0; j < this.width; j++) {
+        result += c || 'X';
+      }
+      console.log(result);
+    }
+  }
+}
+
+module.exports = Square;

18324f3cc1285aece6d17b11414c0d2df4c12c32
memesofting <bibitayoadekunle00@gmail.com>
2024-06-27 15:18:58 +0100

    corrected shebang

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,29 +1,33 @@
-#!/usr/bin/bash
+#!/usr/bin/python3
 import MySQLdb
 import sys
 
 def list_states(mysql_username, mysql_password, database_name):
-    db = MySQLdb.connect(
-        host="localhost",
-        port=3306,
-        user=mysql_username,
-        passwd=mysql_password,
-        db=database_name
-        )
-    cursor = db.cursor()
-    cursor.execute("SELECT id, name FROM states ORDER BY id ASC")
+        db = MySQLdb.connect(
+            host="localhost",
+            port=3306,
+            user=mysql_username,
+            passwd=mysql_password,
+            db=database_name
+            )
+        cursor = db.cursor()
+        cursor.execute("SELECT id, name FROM states ORDER BY id ASC")
+        
+        rows = cursor.fetchall()
+        
+        for row in rows:
+            for col in row:
+                print ("%s," % col)
+            """"print(row)""""
     
-    rows = cursor.fetchall()
+        cursor.close()
+        db.close()
     
-    for row in rows:
-        print(row)
-    
-    cursor.close()
-    db.close()
-    
-if __name == "__main__":
+if __name__ == "__main__":
     if len(sys.argv) == 4:
         mysql_username = sys.argv[1]
         mysql_password = sys.argv[2]
         database_name = sys.argv[3]
         list_states(mysql_username, mysql_password, database_name)
+    else:
+        print("Usage: python script.py <mysql_username> <mysql_password> <database_name>")

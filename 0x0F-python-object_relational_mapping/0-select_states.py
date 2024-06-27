ec5a74af2892b920dc9df436bf3ef27a4faa0e90
memesofting <bibitayoadekunle00@gmail.com>
2024-06-27 15:49:45 +0100

    missing passwd

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -2,32 +2,33 @@
 import MySQLdb
 import sys
 
+
 def list_states(mysql_username, mysql_password, database_name):
-        db = MySQLdb.connect(
-            host="localhost",
-            port=3306,
-            user=mysql_username,
-            passwd=mysql_password,
-            db=database_name
-            )
-        cursor = db.cursor()
-        cursor.execute("SELECT id, name FROM states ORDER BY id ASC")
-        
-        rows = cursor.fetchall()
-        
-        for row in rows:
-            for col in row:
-                print ("%s," % col)
-            """"print(row)""""
-    
-        cursor.close()
-        db.close()
-    
+    """lists all states from the database hbtn_0c_0_usa"""
+
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=mysql_username,
+        passwd=mysql_password,
+        db=database_name
+        )
+    cursor = db.cursor()
+    cursor.execute("SELECT id, name FROM states ORDER BY id ASC")
+    rows = cursor.fetchall()
+    for row in rows:
+        for col in row:
+            print(row)
+    cursor.close()
+    db.close()
+
+
 if __name__ == "__main__":
     if len(sys.argv) == 4:
         mysql_username = sys.argv[1]
-        mysql_password = sys.argv[2]
-        database_name = sys.argv[3]
+        if len(sys.argv) > 2:
+            mysql_password = sys.argv[2]
+        else:
+            mysql_password = None
+        database_name = sys.argv[-1]
         list_states(mysql_username, mysql_password, database_name)
-    else:
-        print("Usage: python script.py <mysql_username> <mysql_password> <database_name>")

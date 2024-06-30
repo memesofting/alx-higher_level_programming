870737c12410254406b6a729521114b0026d286b
memesofting <bibitayoadekunle00@gmail.com>
2024-06-30 14:26:07 +0100

    added permision

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 """Script lists all state Objects from database"""
-import SQLAlchemy as sa
+import sqlalchemy
 import MySQLdb
+from sqlalchemy import create_engine
 from model_state import Base, State
 import sys
 
@@ -11,8 +12,23 @@ def all_states(mysql_username, mysql_password, db_name):
 
     connection_string = f'''mysql+mysqldb://
     {mysql_username}:{mysql_password}@localhost:3306/{db_name}'''
-    engine = sa.create_engine(connection_string)
+    engine = create_engine(connection_string)
     Base.metadata.create_all(engine)
+    db = MySQLdb.connect(
+        host='localhost',
+        port=3306,
+        user=mysql_username,
+        passwd=mysql_password,
+        db=db_name
+    )
+    cursor = db.cursor()
+    query = "SELECT * FROM states ORDER BY states.id"
+    cursor.execute(query)
+    states = cursor.fetchall()
+    for state in states:
+        print(state)
+    cursor.close()
+    db.close()
 
 
 if __name__ == "__main__":

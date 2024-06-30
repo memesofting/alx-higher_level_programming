e28ef7590dc9c9e7eea1433eae7e6f49393a102b
memesofting <bibitayoadekunle00@gmail.com>
2024-06-30 13:10:12 +0100

    creating engine

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,4 +1,18 @@
 #!/usr/bin/python3
-"""Class definition of a state"""
+"""Module defines a state and an instance of
+Base = declaration_base()"""
+import sqlalchemy
+import MySQLdb
+from sqlalchemy import create_engine, Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
 
-class State():
+
+Base = declarative_base()
+
+
+class State(Base):
+    """Class definition of a state, inherits from Base"""
+
+    __tablename__ = 'states'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(128), nullable=False)

d5da2789ea4e1dea8252ca1c4b776f9df9491252
memesofting <bibitayoadekunle00@gmail.com>
2024-06-30 14:37:47 +0100

    removed mysqldb query

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 """Script lists all state Objects from database"""
-import sqlalchemy
 import MySQLdb
 from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
 from model_state import Base, State
 import sys
 
@@ -14,21 +14,16 @@ def all_states(mysql_username, mysql_password, db_name):
     {mysql_username}:{mysql_password}@localhost:3306/{db_name}'''
     engine = create_engine(connection_string)
     Base.metadata.create_all(engine)
-    db = MySQLdb.connect(
-        host='localhost',
-        port=3306,
-        user=mysql_username,
-        passwd=mysql_password,
-        db=db_name
-    )
-    cursor = db.cursor()
-    query = "SELECT * FROM states ORDER BY states.id"
-    cursor.execute(query)
-    states = cursor.fetchall()
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    states = session.query(State).order_by(State.id).all()
+
     for state in states:
-        print(state)
-    cursor.close()
-    db.close()
+        print(f"{state.id}: {state.name}")
+
+    session.close()
 
 
 if __name__ == "__main__":

0307da3bf0f40d7677561c934be51654150732f0
memesofting <bibitayoadekunle00@gmail.com>
2024-07-19 10:53:47 +0100

    get request

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Script sends a GET request to a url and displays size of the body of the response
-curl -s -w "200" "$1" | wc -c
+# Script sends a GET request to a url and displays size of the body of 200 status code response
+curl -s -o >(cat) -w "%{http_code}" "$1" | { read -r status; [ "$status" -eq 200 ] && cat; }
